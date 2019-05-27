25ae842cceb27c747892a80a173a12285a11a8ce
Anders Jurisoo <jurisoo@hotmail.com>
2019-05-27 11:00:28 +0200

    Fixed url path

--- src/resources/js/components/WorkspaceTabs/Build/BuildTab.vue
+++ src/resources/js/components/WorkspaceTabs/Build/BuildTab.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="flex flex-col max-w-md mx-auto px-8 bg-white pt-4 h-full">        
-        <img v-if="!this.results.length" src="/img/build.gif">
+        <img v-if="!this.results.length" src="/vendor/ajthinking/skeleton/img/build.gif">
 
         <div class="flex flex-col mt-8 text-center" v-if="this.results.length">
             <div class="mx-auto my-4 font-semibold p-4 bg-white text-blue text-xs border rounded border-blue shadow">
