3ce8915b3200ac1b989c351d99bafe12be2e1338
Anders Jurisoo <jurisoo@hotmail.com>
2019-05-30 15:12:00 +0200

    Implemented new logo in header

--- src/resources/js/components/AppHeader.vue
+++ src/resources/js/components/AppHeader.vue
@@ -1,14 +1,14 @@
 <template>
 <div class="list-reset flex bg-black p-4">
     <div class="flex flex-1">
-        <div class="flex items-center flex-no-shrink text-white mr-6"
+        <div class="flex items-center flex-no-shrink text-white mr-2"
             @click="$store.dispatch('navigate', {namespace: 'showSplash', tab: true})"
         >
-            <svg class="fill-current h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg"><path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"/></svg>
-            <span class="font-semibold text-xl tracking-tight">Pipe Dream</span>
+            <img src="/vendor/ajthinking/skeleton/img/logo.svg" width="28" height="28" viewBox="0 0 54 54">
+            <span class="font-semibold text-xl tracking-tight ml-2">Pipe Dream</span>
         </div>    
-        <div class="flex mr-6 items-center">
-            <a class="text-sm text-white hover:text-blue-darker no-underline" href="#">{{appName}}</a>
+        <div class="flex mr-2 items-center">
+            <a class="text-sm text-white hover:text-blue-darker no-underline" href="#">/ laravel</a>
         </div>
     </div>
 </div>
