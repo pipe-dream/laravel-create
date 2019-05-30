71a90ced5fa475bf706c6412234f5eae99b41fef
Anders Jurisoo <jurisoo@hotmail.com>
2019-05-30 15:20:51 +0200

    Put FA credits by logo

--- src/resources/js/components/AppHeader.vue
+++ src/resources/js/components/AppHeader.vue
@@ -4,11 +4,14 @@
         <div class="flex items-center flex-no-shrink text-white mr-2"
             @click="$store.dispatch('navigate', {namespace: 'showSplash', tab: true})"
         >
+            <!-- modified from: https://fontawesome.com/icons/ring?style=solid thanks! -->
             <img src="/vendor/ajthinking/skeleton/img/logo.svg" width="28" height="28" viewBox="0 0 54 54">
-            <span class="font-semibold text-xl tracking-tight ml-2">Pipe Dream</span>
+            <a title="github" href="https://github.com/ajthinking" target="_blank" class="hover:text-blue-darker no-underline text-white">
+                <span class="font-semibold text-xl tracking-tight ml-2">Pipe Dream</span>
+            </a>
         </div>    
         <div class="flex mr-2 items-center">
-            <a class="text-sm text-white hover:text-blue-darker no-underline" href="#">/ laravel</a>
+            <a class="text-sm text-white hover:text-blue-darker no-underline" href="https://github.com/laravel/laravel" target="_blank">/ laravel</a>
         </div>
     </div>
 </div>
