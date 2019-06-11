35501b864ecc09be6a85d73a2fe46e95f89828c8
Anders Jurisoo <jurisoo@hotmail.com>
2019-06-11 16:07:35 +0200

    Added quotes to password

--- src/resources/js/fileFactories/Laravel/pipes/FactoryPipe.js
+++ src/resources/js/fileFactories/Laravel/pipes/FactoryPipe.js
@@ -40,7 +40,7 @@ export default class FactoryPipe extends ModelPipe {
                 slug: "$faker->slug",
                 title: "$faker->words(3, true)",
                 email: "$faker->unique()->safeEmail",
-                password: "$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi", // password
+                password: "'$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi'", // password
                 remember_token: "Str::random(10)",
                 ... { /* add more common names here */},
                 default: "$faker->sentence()"
