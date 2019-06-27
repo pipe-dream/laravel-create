3075052fa6216fc414769ae8f4613788c8b25882
Anders Jurisoo <jurisoo@hotmail.com>
2019-06-27 14:12:10 +0200

    Simplified app/store

--- src/resources/js/app.js
+++ src/resources/js/app.js
@@ -1,12 +1,24 @@
-import store from './store.js'
 import Vue from 'vue'
+import Vuex from 'vuex'
+import PipeDream from 'core'
 import { PipeDreamVueTools } from 'core'
+import LaravelFileFactory from 'laravel-file-factory';
 
+/* setup Vue to use Vuex and Pipe Dream components */
 window.Vue = Vue
-window.store = store
-
+Vue.use(Vuex)
 Vue.use(PipeDreamVueTools)
+Vue.config.debug = true
 
+/* Create Pipe Dream default store
+ * Attach it to window to make it accessible inside core and file factories */
+window.store = new Vuex.Store(
+    new PipeDream({
+        fileFactory: LaravelFileFactory
+    }).defaultStore
+)
+
+/* Let's go */
 new Vue({
     el: '#app',
     store
