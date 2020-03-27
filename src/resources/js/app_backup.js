57daae9fde73b3b56046ef2e0b65d0b582c851f1
Kasper Laukamp <kl@KEMENTEC.local>
2020-03-27 10:31:24 +0100

    app.js are now overwritten by app_backup.js when the compilation are done

---
+++ src/resources/js/app_backup.js
@@ -0,0 +1,33 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import {PipeDream} from '@pipe-dream/core'
+import {Schema} from "@pipe-dream/core/";
+import {PipeDreamVueTools} from '@pipe-dream/core'
+/* imports will be automatically added */
+/* setup Vue to use Vuex and Pipe Dream components */
+Vue.use(Vuex)
+Vue.use(PipeDreamVueTools)
+Vue.config.debug = true
+window.Vue = Vue
+
+/* Create Pipe Dream default store
+ * Attach it to window to make it accessible inside core and file factories */
+
+window.store = new Vuex.Store(
+    new PipeDream({
+        fileFactories: [
+            /* file factories will be automatically added */
+        ],
+        ...window.__ENV__
+    }).defaultStore
+)
+
+/* Let's go */
+new Vue({
+    el: '#app',
+    store,
+    mounted(){
+        //console.log(Schema.refresh(true))
+        store.dispatch('compileSchema', Schema);
+    }
+})
